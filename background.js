8786a2e2e642ac329469b554db6b653f2866496c
Cenk Alti <cenkalti@gmail.com>
2017-03-08 15:49:42 +0300

    refactor

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
-function getClickHandler() {
-  return function(info, tab) {
-    var url = 'https://app.put.io/magnet?url=' + encodeURIComponent(info.linkUrl);
-    browser.tabs.create({ url: url});
-  };
-};
+function onClick(info, tab) {
+  var url = 'https://app.put.io/magnet?url=' + encodeURIComponent(info.linkUrl);
+  browser.tabs.create({ url: url});
+}
 
 browser.contextMenus.create({
   "title" :browser.i18n.getMessage("downloadMenuItem"),
-  "type" : "normal",
   "contexts" : ["link"],
-  "onclick" : getClickHandler()
+  "onclick" : onClick
 });
